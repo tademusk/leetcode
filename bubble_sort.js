64e96548ab353d44cb065e873a4c7c3403bc3c4a
tademusk <88289170+tademusk@users.noreply.github.com>
2022-10-07 23:20:04 +0300

    Create bubble_sort.js

---
+++ bubble_sort.js
@@ -0,0 +1,54 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'countSwaps' function below.
+ *
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function countSwaps(a) {
+    var n = a.length
+    var temp
+    var count=0
+    for(let i=0; i<n; i++){
+        for(let j=0; j<n-1; j++){
+            if (a[j]>a[j+1]){
+               temp=a[j]
+               a[j]=a[j+1] 
+               a[j+1]=temp
+               count++
+            }
+        }
+    }
+console.log(`Array is sorted in ${count} swaps.`)
+console.log(`First Element: ${a[0]}`)
+console.log(`Last Element: ${a[n-1]}`)
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    countSwaps(a);
+}
