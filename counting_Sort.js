bee33df34229116e8942ce0108f64aee71b2b672
tademusk <88289170+tademusk@users.noreply.github.com>
2022-10-11 15:10:39 +0300

    Create counting_Sort.js

---
+++ counting_Sort.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'countingSort' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function countingSort(arr) {
+    var i, frequency_arr=[]
+    for (i = 0; i<100; i++) {
+        frequency_arr[i] = 0
+    }
+    for (i = 0; i < arr.length; i++) {
+        frequency_arr[arr[i]]++
+    }
+    return frequency_arr
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = countingSort(arr);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
