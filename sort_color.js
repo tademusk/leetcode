6e4481bdb22accddcc0c439ef24aff9ad9ae2d3e
tademusk <88289170+tademusk@users.noreply.github.com>
2022-10-12 07:41:15 +0300

    Create sort_color.js

---
+++ sort_color.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    for( i= 0; i < nums.length; i++){
+        for(let j = 0; j< nums.length-1-i; j++){
+            if(nums[j]>nums[j+1]){
+               [nums[j],nums[j+1]] = [nums[j+1],nums[j]]
+            }
+        }
+    }
+};
