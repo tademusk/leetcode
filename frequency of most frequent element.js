4fc7d1d0e5e3e5e25ea2352bf00508dc37bd4dfb
tademusk <88289170+tademusk@users.noreply.github.com>
2022-10-13 16:49:11 +0300

    Create frequency of most frequent element.js

---
+++ frequency of most frequent element.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+ var maxFrequency = function(nums, k) {
+        nums.sort((a,b)=> a - b);
+        let n = nums.length, sum = 0, ct = 1, end = n - 1, maxfreq = 1;
+        let front = end - 1;
+    
+        while(front >= 0)
+        {
+            sum += nums[end] - nums[front];
+            if(sum <= k)
+            {
+                ct++;
+                maxfreq = Math.max(ct,maxfreq);
+                front--;
+            }
+            else
+            {
+                let size = 1;
+                while(sum > k)
+                {
+                    while(end > 0 && nums[end] == nums[end - 1]) end--; //for duplicate elements
+                    if(end > 0)
+                        end--;
+                    let diff = nums[end + 1] - nums[end];
+                    size = end - front + 1;
+                    sum = sum - size*diff;
+                }
+                ct = size;
+                front--;
+               
+             }
+        
+        }
+        return maxfreq;
+};
