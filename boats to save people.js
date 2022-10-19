8b3b526f36b8077b478a4dd67cd6c8b4ce75278a
tademusk <88289170+tademusk@users.noreply.github.com>
2022-10-19 14:13:52 +0300

    Create boats to save people.js

---
+++ boats to save people.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} people
+ * @param {number} limit
+ * @return {number}
+ */
+var numRescueBoats = function(people, limit) {
+    people.sort((a, b) => a - b);
+    let small = 0;
+    let big = people.length -1 ;
+    let boat = 0;
+    while(small<=big){
+        if(people[small]+people[big]<=limit) small++; 
+        big--;
+        boat++;
+    }
+    return boat;
+};
